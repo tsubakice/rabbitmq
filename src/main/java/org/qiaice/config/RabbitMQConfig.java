057ab906f4fba2574466ee4a082a9ad49fda23b6
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-05-22 16:52:22 +0800

    仅用 @RabbitListener 一个注解描述交换机与队列的绑定关系

--- src/main/java/org/qiaice/config/RabbitMQConfig.java
+++ src/main/java/org/qiaice/config/RabbitMQConfig.java
@@ -1,8 +1,6 @@
 package org.qiaice.config;
 
-import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +10,5 @@ public class RabbitMQConfig {
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean(value = "direct")
-    public Exchange exchange() {
-        return ExchangeBuilder.directExchange("amq.direct").build();
-    }
-
-    @Bean(value = "queue1")
-    public Queue queue() {
-        return QueueBuilder.nonDurable("queue1").build();
-    }
-
-    @Bean(value = "direct-queue1")
-    public Binding binding(
-            @Qualifier(value = "direct") Exchange exchange,
-            @Qualifier(value = "queue1") Queue queue
-    ) {
-        return BindingBuilder.bind(queue).to(exchange).with("queue1").noargs();
     }
 }
--- src/main/java/org/qiaice/listener/RabbitMQListener.java
+++ src/main/java/org/qiaice/listener/RabbitMQListener.java
@@ -1,15 +1,28 @@
 package org.qiaice.listener;
 
+import com.rabbitmq.client.Channel;
 import org.qiaice.entity.User;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class RabbitMQListener {
 
-    @RabbitListener(queues = "queue1", messageConverter = "jackson2JsonMessageConverter")
-    public String receive(User user) {
-        System.out.println("Received message: " + user);
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "queue1", durable = "true"),
+            exchange = @Exchange(value = "amq.direct"),
+            key = "queue1"),
+            messageConverter = "jackson2JsonMessageConverter")
+    public String receive(User user, Message message, Channel channel) {
+        System.out.println("Received user: " + user);
+        System.out.println("Received message: " + new String(message.getBody(), StandardCharsets.UTF_8));
+        System.out.println("Received channel: " + channel.getChannelNumber());
         return "响应成功";
     }
 }
